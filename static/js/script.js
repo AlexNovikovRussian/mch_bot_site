c72975bd7fd6fe5a88b8740e4264df93057076ad
AlexNovikovRussian <alexprogramer@mail.ru>
2020-04-27 21:50:20 +0300

    00

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,20 @@
+$(".logout").click((event) => {
+	$(".button-collapse").sideNav("hide")
+	Swal.fire({
+		title: 'Are you sure?',
+		text: "You won't be able to revert this!",
+		icon: 'warning',
+		showCancelButton: true,
+		confirmButtonColor: '#3085d6',
+		cancelButtonColor: '#d33',
+		confirmButtonText: 'Yes, log out!'
+	  }).then((result) => {
+		if (result.value) {
+		  document.location = $("#logout_url").text()
+		}
+	  })
+});
+
 $('document').ready(function(){
 	$('.modal').modal();
 	$('.button-collapse').sideNav();
